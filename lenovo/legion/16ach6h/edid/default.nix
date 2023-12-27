6158462c0bd5d8f987befbf5ddc1d7cd6042b6bc
Felix Ulonska <git@webfoo.de>
2023-12-27 01:32:27 +0100

    commented kernelParams

--- lenovo/legion/16ach6h/edid/default.nix
+++ lenovo/legion/16ach6h/edid/default.nix
@@ -11,5 +11,5 @@ in
   hardware.firmware = [ chip_edid ];
 
   boot.kernelParams = [ "drm.edid_firmware=edid/16ach6h.bin" ];
-  boot.initrd.extraFiles."lib/firmware/edid/16ach6h.bin".source = pkgs.runCommandLocal "chip_edid" { } "cp ${./16ach6h.bin} $out";
+  #boot.initrd.extraFiles."lib/firmware/edid/16ach6h.bin".source = pkgs.runCommandLocal "chip_edid" { } "cp ${./16ach6h.bin} $out";
 }
